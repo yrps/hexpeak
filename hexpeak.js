baa160bece32d7dee66df227ce6cd5c4afcb2dfe
ncoop <nate.cooper.90@gmail.com>
2014-07-21 02:00:39 -0700

    basic validation and cross-update of input fields

--- hexpeak.js
+++ hexpeak.js
@@ -1,2 +1,23 @@
+var numTFs = [];
+
+/** object prototype that parses a textfield and updates others */
+function NumberTF(id, rad) {
+    this.elem = $( id );
+    this.radix = rad;
+    $( id ).blur(function() {
+        var num = parseInt($(this).val(), rad);
+        num = isNaN(num) ? 0 : num;
+        $.each(numTFs, function(k, v){
+            v.elem.val(num.toString(v.radix));
+        })
+    });
+}
+
 $(document).ready(function() {
+    numTFs = [
+        new NumberTF("#decfield", 10),
+        new NumberTF("#binfield", 2),
+        new NumberTF("#octfield", 8),
+        new NumberTF("#hexfield", 16)
+    ];
 });
