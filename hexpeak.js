f2573ea44621457901aaf84e5ef499d97ad406f3
ncoop <nate.cooper.90@gmail.com>
2014-07-23 20:31:39 -0700

    closing #7, closing #8

--- hexpeak.js
+++ hexpeak.js
@@ -1,4 +1,13 @@
+var playing = false;
 var numTFs = [];
+
+/** function to initialize the fields to a random value */
+function initFields(min, max) {
+    min = min || 0x10;
+    max = max || 0xFFF;
+    var randVal = Math.floor(Math.random() * (max-min) + min);
+    updateTFs(randVal);
+}
 
 /** object prototype that parses a textfield and updates others */
 function NumberTF(id, rad) {
@@ -7,10 +16,15 @@ function NumberTF(id, rad) {
     $( id ).change(function() {
         var num = parseInt($(this).val(), rad);
         num = isNaN(num) ? 0 : num;
-        $.each(numTFs, function(k, v){
-            v.elem.val(num.toString(v.radix).toUpperCase());
-        })
+        updateTFs(num)
     });
+}
+
+/** function to synchronize all fields according to their respective bases */
+function updateTFs(num) {
+    $.each(numTFs, function(k, v){
+        v.elem.val(num.toString(v.radix).toUpperCase());
+    })
 }
 
 $(document).ready(function() {
@@ -20,4 +34,6 @@ $(document).ready(function() {
         new NumberTF("#octfield", 8),
         new NumberTF("#hexfield", 16)
     ];
+    initFields();
+//    console.log($(numTFs[0].elem).val())
 });
